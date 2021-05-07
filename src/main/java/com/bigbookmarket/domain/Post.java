1f7511a5c479e694ecaf9df41f8ed8161566c22e
congcoding <congcoding@gmail.com>
2021-05-07 13:08:59 +0900

    Post, PostRepository 추가

---
+++ src/main/java/com/bigbookmarket/domain/Post.java
@@ -0,0 +1,39 @@
+package com.bigbookmarket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @Column(columnDefinition = "BIGINT")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bookId", referencedColumnName = "bookId")
+    private Book book;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Byte category;
+
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+}
+
