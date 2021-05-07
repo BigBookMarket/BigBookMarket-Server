b92bff2e29e3688063a998ee21ced546d64b1e08
congcoding <congcoding@gmail.com>
2021-05-07 13:31:51 +0900

    ApiResponse 삭제

--- src/main/java/com/bigbookmarket/common/ResponseMessage.java
+++
@@ -1,9 +0,0 @@
-package com.bigbookmarket.common;
-
-public class ResponseMessage {
-
-    public static final String READ_BOOK_CATEGORY = "Category 정보 조회 성공";
-
-    public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
-}
-
