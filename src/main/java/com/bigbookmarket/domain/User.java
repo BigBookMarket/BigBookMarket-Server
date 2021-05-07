fe1c22b48d72d1f0e51c4c9918c5a1ee7aeeadc6
congcoding <congcoding@gmail.com>
2021-05-07 12:40:44 +0900

    비밀번호 암호화할 때 길이가 길어져서 User domain pwd 길이 수정

--- src/main/java/com/bigbookmarket/domain/User.java
+++ src/main/java/com/bigbookmarket/domain/User.java
@@ -2,17 +2,18 @@ package com.bigbookmarket.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Getter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class User {
+
     @Id
     @Column(columnDefinition = "BIGINT")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +22,7 @@ public class User {
     @Column(columnDefinition = "CHAR(10)", nullable = false)
     private String id;
 
-    @Column(columnDefinition = "CHAR(20)", nullable = false)
+    @Column(columnDefinition = "CHAR(60)", nullable = false)
     private String pwd;
 
     @Column(columnDefinition = "CHAR(11)", nullable = false)
