da7d925ecdaed2a484bdb1b57b87b8eafb3d6b64
congcoding <congcoding@gmail.com>
2021-05-07 13:36:35 +0900

    Swagger 추가

--- src/main/java/com/bigbookmarket/Application.java
+++ src/main/java/com/bigbookmarket/Application.java
@@ -1,9 +1,14 @@
 package com.bigbookmarket;
 
+import com.bigbookmarket.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
