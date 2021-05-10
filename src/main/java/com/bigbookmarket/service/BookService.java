06fc30186796d0e3879cb9dc63c1e57275f94446
congcoding <congcoding@gmail.com>
2021-05-10 16:32:12 +0900

    전체 Book 조회하는 API 추가

--- src/main/java/com/bigbookmarket/service/BookService.java
+++ src/main/java/com/bigbookmarket/service/BookService.java
@@ -1,17 +1,25 @@
 package com.bigbookmarket.service;
 
 import com.bigbookmarket.domain.BookRepository;
+import com.bigbookmarket.web.dto.BookListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class BookService {
 
     private final BookRepository bookRepository;
+
+    public List<BookListResponseDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(BookListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     @Transactional(readOnly = true)
     public List<String> findCategory() {
