8feca30972937adc73fcabb8385527303d8d2c63
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2021-05-10 15:44:46 +0900

    Buy, Comment, Item, Message 추가

--- src/main/java/com/bigbookmarket/domain/ItemTradingMethodConverter.java
+++ src/main/java/com/bigbookmarket/domain/ItemTradingMethodConverter.java
@@ -1,18 +1,22 @@
 package com.bigbookmarket.domain;
 
-//@Converter(autoApply = true)
-//public class ItemTradingMethodConverter implements AttributeConverter<ItemTradingMethod, Integer> {
-//
-//    @Override
-//    public Integer convertToDatabaseColumn(ItemTradingMethod tradingMethod) {
-//        return tradingMethod.value;
-//    }
-//
-//    @Override
-//    public ItemTradingMethod convertToEntityAttribute(Integer integer) {
-//        return Stream.of(ItemTradingMethod.values())
-//                .filter(c -> c.value == integer)
-//                .findFirst()
-//                .orElseThrow(IllegalArgumentException::new);
-//    }
-//}
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class ItemTradingMethodConverter implements AttributeConverter<ItemTradingMethod, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(ItemTradingMethod tradingMethod) {
+        return tradingMethod.value;
+    }
+
+    @Override
+    public ItemTradingMethod convertToEntityAttribute(Integer integer) {
+        return Stream.of(ItemTradingMethod.values())
+                .filter(c -> c.value == integer)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
