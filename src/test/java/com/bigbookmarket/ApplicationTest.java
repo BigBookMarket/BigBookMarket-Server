b02876fa36a7c438cd82131c4e2b673f9f688cc3
congcoding <congcoding@gmail.com>
2021-05-14 21:11:26 +0900

    Test Code 삭제

--- src/test/java/com/bigbookmarket/ApplicationTest.java
+++ src/test/java/com/bigbookmarket/ApplicationTest.java
@@ -1,16 +1,12 @@
 package com.bigbookmarket;
 
-import com.bigbookmarket.domain.Item;
 import com.bigbookmarket.domain.ItemRepository;
-import com.bigbookmarket.domain.Post;
 import com.bigbookmarket.domain.PostRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,11 +18,11 @@ public class ApplicationTest {
 
     @Test
     public void itemTest() {
-        List<Item> list = itemRepository.findAll();
-        List<Post> list2 = postRepository.findAllDesc();
-        System.out.println(list.get(0).getMethod());
-        System.out.println(list.get(0).getStatus());
-        System.out.println(list2.get(0).getPostId());
+//        List<Item> list = itemRepository.findAll();
+//        List<Post> list2 = postRepository.findAllDesc();
+//        System.out.println(list.get(0).getMethod());
+//        System.out.println(list.get(0).getStatus());
+//        System.out.println(list2.get(0).getPostId());
     }
 
 }
