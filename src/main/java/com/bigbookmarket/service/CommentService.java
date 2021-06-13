0a1e09ed014ac5843ea3facb955d99d13dadf71d
congcoding <congcoding@gmail.com>
2021-06-13 18:31:56 +0900

    CommentSaveeRequestDto userId를 id로 수정

--- src/main/java/com/bigbookmarket/service/CommentService.java
+++ src/main/java/com/bigbookmarket/service/CommentService.java
@@ -1,6 +1,10 @@
 package com.bigbookmarket.service;
+
 import com.bigbookmarket.domain.CommentRepository;
-import com.bigbookmarket.web.dto.*;
+import com.bigbookmarket.domain.User;
+import com.bigbookmarket.domain.UserRepository;
+import com.bigbookmarket.web.dto.CommentListResponseDto;
+import com.bigbookmarket.web.dto.CommentSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +16,14 @@ import java.util.stream.Collectors;
 @Service
 public class CommentService {
 
+    private final UserRepository userRepository;
     private final CommentRepository commentRepository;
 
     @Transactional
     public Long save(CommentSaveRequestDto requestDto) {
-        return commentRepository.save(requestDto.toEntity()).getCommentId();
+        User user = userRepository.findById(requestDto.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 User가 없습니다. id = " + requestDto.getId()));
+        return commentRepository.save(requestDto.toEntity(user)).getCommentId();
     }
 
     @Transactional(readOnly = true)
--- src/main/java/com/bigbookmarket/web/dto/CommentSaveRequestDto.java
+++ src/main/java/com/bigbookmarket/web/dto/CommentSaveRequestDto.java
@@ -1,6 +1,5 @@
 package com.bigbookmarket.web.dto;
 
-import com.bigbookmarket.domain.Book;
 import com.bigbookmarket.domain.Comment;
 import com.bigbookmarket.domain.Post;
 import com.bigbookmarket.domain.User;
@@ -12,13 +11,13 @@ import lombok.NoArgsConstructor;
 public class CommentSaveRequestDto {
 
     private Long postId;
-    private Long userId;
+    private String id;
     private String content;
 
-    public Comment toEntity() {
+    public Comment toEntity(User user) {
         return Comment.builder()
                 .post(Post.builder().postId(postId).build())
-                .user(User.builder().userId(userId).build())
+                .user(user)
                 .content(content)
                 .build();
     }
